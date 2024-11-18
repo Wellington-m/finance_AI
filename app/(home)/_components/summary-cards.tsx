a5d061477ae2b074cbf5b4772421430f2cffec23
Wellington-m <wellingtonmachioni@gmail.com>
2024-11-18 17:36:42 -0300

    feat: add time select

--- app/(home)/_components/summary-cards.tsx
+++ app/(home)/_components/summary-cards.tsx
@@ -7,11 +7,22 @@ import {
 import SummaryCard from "./summary-card";
 import { db } from "@/app/_lib/prisma";
 
-const SummaryCards = async () => {
+interface SummaryCards {
+  month: string;
+}
+
+const SummaryCards = async ({ month }: SummaryCards) => {
+  const where = {
+    date: {
+      gte: new Date(`2024-${month}-01`),
+      lt: new Date(`2024-${month}-31`),
+    },
+  };
+
   const depositsTotal = Number(
     (
       await db.transaction.aggregate({
-        where: { type: "DEPOSIT" },
+        where: { ...where, type: "DEPOSIT" },
         _sum: { amount: true },
       })
     )?._sum?.amount,
@@ -19,7 +30,7 @@ const SummaryCards = async () => {
   const investimentsTotal = Number(
     (
       await db.transaction.aggregate({
-        where: { type: "INVESTIMENT" },
+        where: { ...where, type: "INVESTIMENT" },
         _sum: { amount: true },
       })
     )?._sum?.amount,
@@ -27,7 +38,7 @@ const SummaryCards = async () => {
   const expensesTotal = Number(
     (
       await db.transaction.aggregate({
-        where: { type: "EXPENSE" },
+        where: { ...where, type: "EXPENSE" },
         _sum: { amount: true },
       })
     )?._sum?.amount,
