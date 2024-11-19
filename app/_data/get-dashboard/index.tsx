9c6acb9b22741d4b589c088ceab1c34d009509a4
Wellington-m <wellingtonmachioni@gmail.com>
2024-11-19 15:41:05 -0300

    feat: ensure getDashboard is protected

--- app/_data/get-dashboard/index.tsx
+++ app/_data/get-dashboard/index.tsx
@@ -1,9 +1,17 @@
 import { db } from "@/app/_lib/prisma";
 import { TransactionType } from "@prisma/client";
 import { TotalExpensePerCategory, TransactionPercentagePerType } from "./type";
+import { auth } from "@clerk/nextjs/server";
 
 export const getDashboard = async (month: string) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
   const where = {
+    userId,
     date: {
       gte: new Date(`2024-${month}-01`),
       lt: new Date(`2024-${month}-31`),
@@ -78,7 +86,7 @@ export const getDashboard = async (month: string) => {
   const lastTransactions = await db.transaction.findMany({
     where,
     orderBy: { date: "desc" },
-    take: 15,
+    take: 10,
   });
 
   return {
